8bf8e4a7e1ac2e0cc3a2c85d3113e78ca8063f08
CarlosAlbertoGarciaRamirezADSO <calbertogarciaramirez123@gmail.com>
2024-03-09 00:22:13 -0500

    Ejercicios 50

--- javascriptEjercicios/ejercicio49.js
+++ javascriptEjercicios/ejercicio49.js
@@ -1,2 +1,12 @@
 // Escriba un algoritmo que calcule el área de un rectángulo siempre y cuando los lados sean 
 // positivos: área triangulo= lado * lado 
+
+let lado1 = parseInt(prompt("Ingrese el lado 1: "));
+let lado2 = parseInt(prompt("Ingrese el lado 2: "));
+
+if(lado1 >0 && lado2 >0){
+    let area = lado1 * lado2;
+    alert(`El area de el rectangulo es ${area}`)
+}else{
+    alert("No se aceptan numeros negativos");
+}
